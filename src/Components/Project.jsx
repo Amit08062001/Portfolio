8c7b13407fec713b621b3655f5c6943da028ecc2
Amit08062001 <amitsingh19052001@gmail.com>
2024-07-27 15:03:22 +0530

    added one more project

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Project.css';
 import project1Image from '../assets/project1.png'; 
+import project2Image from '../assets/project2Image'
 
 
 const Project = () => {
@@ -14,12 +15,14 @@ const Project = () => {
           <h3>MelodyStream</h3>
           <p>MelodyStream is a modern music streaming web application designed to offer a seamless and interactive user experience. The application features a clean and intuitive interface where users can easily navigate through different sections, play songs, and enjoy smooth transitions and animations.</p>
           <a className='github' href='https://github.com/Amit08062001/MelodyStream1'>Github</a>
+          <a className='github' href='https://amit08062001.github.io/MelodyStream1/'>Live</a>
+
         </div>
         <div className="project-card">
-          <img src={project1Image} alt="Arrowai Industries 2022" />
-          <h3>MelodyStream</h3>
-          <p>MelodyStream is a modern music streaming web application designed to offer a seamless and interactive user experience. The application features a clean and intuitive interface where users can easily navigate through different sections, play songs, and enjoy smooth transitions and animations.</p>
-          <a className='github' href='https://github.com/Amit08062001/MelodyStream1'>Github</a>
+          <img src={project2Image} alt="Arrowai Industries 2022" />
+          <h3>WheatherApp</h3>
+          <p>A weather widget that fetches data from the OpenWeatherMap API, displaying dynamic background images and weather details like temperature and wind. It uses HTML, CSS, and JavaScript .</p>
+          <a className='github' href='https://github.com/Amit08062001/WheatherApp/'>Github</a>
           <a className='github' href='https://amit08062001.github.io/WheatherApp/'>Live</a>
         </div>
       </div>
