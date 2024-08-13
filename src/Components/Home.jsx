85ff3876f111bfdbb77bf1963532e9992df194c0
Amit08062001 <amitsingh19052001@gmail.com>
2024-08-13 23:36:30 +0530

    colur changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
           <div className='container'>
             <p className='text'>Frontend Developer</p>
           </div>
-          <p className='home-p-2'>Hi, I’m a Frontend Developer specializing in React. I’m passionate about creating dynamic, user-friendly web applications. With hands-on experience in React, modern JavaScript, HTML, and CSS, I build responsive and visually appealing interfaces. I thrive on turning design concepts into functional, high-performance applications and enjoy staying updated with the latest industry trends and technologies. I’m always looking for new challenges to push my skills further. Check out my portfolio to see my work, and feel free to reach out for potential collaborations or to discuss ideas!</p>
+          <p className='home-p-2'>Hi, I’m a Frontend Developer specializing in React. I’m passionate about creating dynamic, user-friendly web applications. With hands-on experience in React, modern JavaScript, HTML, and CSS, I build responsive and visually appealing interfaces. </p>
           <button className='resume'><a href= {resume}>Download Resume </a></button>
         </div>
         <div className="home-image-container">
