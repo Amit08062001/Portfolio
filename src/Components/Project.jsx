9b70a5fdff206c932d4ae4ceeb169eeb6f6e6a64
Amit08062001 <amitsingh19052001@gmail.com>
2024-08-22 19:51:00 +0530

    changes in projects , contact, experience

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -12,7 +12,7 @@ const Project = () => {
       <div className="project-grid">
         <div className="project-card">
           <img src={project1Image} alt="project" />
-          <h3>MelodyStream</h3>
+          <h3 className='projrct-card-h'>MelodyStream</h3>
           <p>MelodyStream is a modern music streaming web application designed to offer a seamless and interactive user experience. The application features a clean and intuitive interface where users can easily navigate through different sections, play songs, and enjoy smooth transitions and animations.</p>
           <a className='github' href='https://github.com/Amit08062001/MelodyStream1'>Github</a>
           <a className='github' href='https://amit08062001.github.io/MelodyStream1/'>Live</a>
@@ -20,14 +20,14 @@ const Project = () => {
         </div>
         <div className="project-card">
           <img src={project2Image} alt="project" />
-          <h3>WheatherApp</h3>
+          <h3 className='projrct-card-h'>WheatherApp</h3>
           <p>A weather widget that fetches data from the OpenWeatherMap API, displaying dynamic background images and weather details such as temperature, 'feels like' temperature, and wind speed. It features a responsive design with HTML and CSS for styling, and JavaScript for handling API interactions.</p>
           <a className='github' href='https://github.com/Amit08062001/WheatherApp/'>Github</a>
           <a className='github' href='https://amit08062001.github.io/WheatherApp/'>Live</a>
         </div>
         <div className="project-card">
           <img src={project3Image} alt="project" />
-          <h3>Fit Raho</h3>
+          <h3 className='projrct-card-h' >Fit Raho</h3>
           <p>Fit Raho is a dynamic and interactive fitness and wellness platform dedicated to empowering users to lead healthier and more active lives. The site offers a holistic approach to fitness, integrating personalized workout plans and comprehensive nutrition guides to support users health goals.</p>
           <a className='github' href='https://github.com/Amit08062001/Fit-Raho'>Github</a>
           <a className='github' href='https://amit08062001.github.io/Fit-Raho/'>Live</a>
